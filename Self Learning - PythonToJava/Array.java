23584ddde5e467942f3e449b9e93573f56f1bda2
EmadNama <emadnama@hotmail.com>
2025-02-20 14:49:36 +0200

    Array

---
+++ Self Learning - PythonToJava/Array.java
@@ -0,0 +1,14 @@
+public class Array {
+    public static void main(String[] args){
+
+        int[] scores = {85,90,75,88,92};
+        System.out.println(scores[0]);
+        scores[0] = 15;
+        System.out.println(scores[0]);
+
+        String[] names = {"Emad", "Daniel", "Jack"};
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
