70f89c2e9f17f7ddbc63e89f4f0d438b76c94cf1
EmadNama <emadnama@hotmail.com>
2025-02-20 13:55:46 +0200

    Practicing Java Syntax - Data Types

---
+++ Self Learning - PythonToJava/DataTypes.java
@@ -0,0 +1,26 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte myByte = 127;
+        short myShort = 32767;
+        int myInt = 2100000000;
+        long myLong = 1000000000000000000L;
+
+        float myFloat = 3.14f;
+        double myDouble = 35.655;
+
+        char myChar = 'A';
+        boolean myBool = true;
+
+        String myString = "Hello, World!";
+
+        System.out.println("byte: " + myByte);
+        System.out.println("short: " + myShort);
+        System.out.println("int: " + myInt);
+        System.out.println("long: " + myLong);
+        System.out.println("float: " + myFloat);
+        System.out.println("double: " + myDouble);
+        System.out.println("char: " + myChar);
+        System.out.println("boolean: " + myBool);
+        System.out.println("String: " + myString);
+    }
+}
