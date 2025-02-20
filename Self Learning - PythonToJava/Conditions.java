8b01331b7a82ee7429e097e83acce5495e9e574a
EmadNama <emadnama@hotmail.com>
2025-02-20 13:56:08 +0200

    Practicing Java Syntax - Conditions.java

---
+++ Self Learning - PythonToJava/Conditions.java
@@ -0,0 +1,21 @@
+public class Conditions {
+    public static void main(String[] args) {
+        byte score = 85;
+        String grade = getGrade(score);
+        System.out.println("Your grade is: " + grade);
+    }
+
+    public static String getGrade(byte score) {
+        if (score >= 90) {
+            return "A";
+        } else if (score >= 80) {
+            return "B";
+        } else if (score >= 70) {
+            return "C";
+        } else if (score >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
