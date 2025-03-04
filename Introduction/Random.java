3e22086f5cdac21fa245914fe07bd692f38c91d4
Imad Nama <emadnama@hotmail.com>
2025-03-04 23:57:54 +0200

    Test

--- Introduction/Random.java
+++ Introduction/Random.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+//class Random{
+//    public static void main(String[] args) {
+//        int number = 1 + (int)(Math.random() * 10);
+//        System.out.println(number);
+//    }
+//}
 
-public class Random {
+import java.util.Scanner;
+
+class Random{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = 1 + (int)(Math.random() * 10);
+        int guess = 0;
+        while (guess != number){
+            System.out.print("Enter your guess: ");
+            guess = scan.nextInt();
+            if (guess == number){
+                System.out.print("Right");
+            }
+        }
+    }
+
 }
