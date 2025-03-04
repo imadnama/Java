770861ac0d7ef72139beb364f2645b91256269b3
Imad Nama <emadnama@hotmail.com>
2025-03-04 22:48:00 +0200

    Switch Statements

---
+++ Self Learning/Fundamentals/NestedLoops.java
@@ -0,0 +1,9 @@
+package Fundamentals;
+
+public class NestedLoops {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++){
+            System.out.println("*".repeat(i));
+        }
+    }
+}
