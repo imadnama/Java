f480390037e70fd558900541922202ed79962b53
Imad Nama <emadnama@hotmail.com>
2025-03-04 23:40:40 +0200

    Intro - While Loops

---
+++ Introduction/While1.java
@@ -0,0 +1,9 @@
+class While1 {
+    public static void main(String[] args) {
+        int i = 0, n = 5;
+        while(i < n) {
+            i++;
+            System.out.println(i);
+        }
+    }
+}
